b350cf347c26ef3edd89411997c091abb3fc710b
Denny Wu <dennywu0315@gmail.com>
2013-06-07 15:44:18 +0800

    Update move1.sh

--- intv/Move10KFile/move1.sh
+++ intv/Move10KFile/move1.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+#编写个shell脚本将当前目录下大于10K的文件转移到/tmp目录下 
+#by DennyW
 
 for FILE in `ls -al| awk '$5>10240 {print $9}'|grep -v sh`
 do
-cp $FILE /tmp/test
+    cp $FILE /tmp/test
 done    
 
 ls -al /tmp/test
