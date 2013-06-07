6653384daf2fb14369ad81248d7cb6e960905477
Denny Wu <dennywu0315@gmail.com>
2013-06-07 15:43:34 +0800

    Update get_net_address.sh

--- intv/GetNetAdd/get_net_address.sh
+++ intv/GetNetAdd/get_net_address.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#编写shell脚本获取本机的网络地址。
+#by DennyW
 
 IP=`ifconfig eth0 |grep 'inet addr'| awk '{print $2}' |awk -F: '{print $2}'`
 NETMASK=`ifconfig eth0 |grep 'inet addr'| awk '{print $4}' |awk -F: '{print $2}'`
